b1faa8380e7403352e13fd6e448b5bf438d34be9
Caesar Ian Belza <moreishi@gmail.com>
2015-07-06 22:24:21 +0800

    Init App

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kissMarryDissApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kissMarryDissApp
+ */
+angular.module('kissMarryDissApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kissMarryDissApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kissMarryDissApp
+ */
+angular.module('kissMarryDissApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
